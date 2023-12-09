d7aa4eff74c5c0490578681b4a925fe09a907559
User <User@WIN-8BH8LKBRQNO>
2023-12-09 23:14:59 +0500

    A class named Person has been opened and properties have been assigned to it

---
+++ Hometask 3/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask_3
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            
+        }
+
+        public void Check(int age)
+        {
+            if (Age > age) 
+            {
+                Console.WriteLine($"Age: {Age}");
+            }
+        }
+
+        public void Check(string name, int age)
+        {
+            if(Age < age)
+            {
+                Console.WriteLine($"Name: {name}\n" +
+                                  $"Age: {age}");
+            }
+        }
+    }
+}
--- Hometask 3/Program.cs
+++ Hometask 3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hometask_3;
+
+Person person = new Person("Saida", 18);
+person.Check("Saida", 3);
+
+
